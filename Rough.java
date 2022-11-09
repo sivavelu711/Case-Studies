23698a2663538dae57ca69c2093555df8eb07738
sivavelu711 <117708044+sivavelu711@users.noreply.github.com>
2022-11-09 20:17:58 +0530

    Add files via upload

---
+++ Rough.java
@@ -0,0 +1,14 @@
+package com.te.restaurantapplication;
+import java.util.Arrays;
+
+import java.util.Scanner;
+
+
+public class Rough extends RuntimeException {
+	
+	String message;
+	
+	public Rough(String message) {
+		super(message);
+	}
+}
